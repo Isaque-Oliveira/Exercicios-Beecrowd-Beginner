bcac3bb9118abcd6e8ac26f2babafbbb08ec8c1e
Isaque Cordeiro de Oliveira <isaque_cordeiro@hotmail.com>
2026-05-08 16:18:41 -0300

    Difference

---
+++ _1007_Difference/src/_1007_Difference/Difference.java
@@ -0,0 +1,23 @@
+package _1007_Difference;
+
+import java.util.Scanner;
+
+public class Difference {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+        
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        int d = sc.nextInt();
+        
+        int dif = a * b - c * d;
+        
+        System.out.println("DIFERENCA = " + dif);
+        
+        sc.close();
+	}
+
+}
